a2380bbddedc1a8b45e3d59186f8bb6498755750
EduardoPembeleAfonso <eduardopembeleafonso@gmail.com>
2025-06-13 15:15:13 +0100

    feat: create new instance from mongodb

--- src/infra/database/mongodb/databaseCon.ts
+++ src/infra/database/mongodb/databaseCon.ts
@@ -1,15 +1,32 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   try {
+//     const { connection } = await mongoose.connect("mongodb://netag:Angola2024%23@95.216.215.24:27017/", {
+//       dbName: "AlloBelleDatabase",
+//     });
+
+//     mongoose.set("debug", true);
+//     console.log(`Server connected to database ${connection.host}`);
+//   } catch (error) {
+//     console.log("Some Error Occurred", error);
+//     process.exit(1);
+//   }
+// }
+
+// databaseCon.ts
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect("mongodb://netag:Angola2024%23@95.216.215.24:27017/", {
+    const { connection } = await mongoose.connect("mongodb://127.0.0.1:27017/", {
       dbName: "AlloBelleDatabase",
     });
 
     mongoose.set("debug", true);
-    console.log(`Server connected to database ${connection.host}`);
+    console.log(`✅ Connected to MongoDB at ${connection.host}`);
   } catch (error) {
-    console.log("Some Error Occurred", error);
+    console.log("❌ Error connecting to MongoDB:", error);
     process.exit(1);
   }
-}
+};
