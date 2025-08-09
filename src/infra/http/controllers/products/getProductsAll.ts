5455f8b24596cf63305f86ed005df78d0c953fa0
EduardoPembeleAfonso <eduardopembeleafonso@gmail.com>
2025-08-09 12:22:10 +0100

    feat: retornando todos os produtos da app

---
+++ src/infra/http/controllers/products/getProductsAll.ts
@@ -0,0 +1,21 @@
+import { Response, Request } from "express";
+import { Products } from "../../../../shared";
+
+export const getProductsAll = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const products = await Products.find({})
+      .populate('category', '_id name')
+      .populate('brand', '_id name')
+      .sort({ timestamp: -1 })
+      .lean();
+
+    return res.status(200).json(products);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Ocorreu um erro ao imprimir lista de produtos." });
+  }
+};
